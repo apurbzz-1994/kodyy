077afb00e6cc0b49792d58c070773dc6fa2da691
apurbzz1994 <akash95august@gmail.com>
2025-01-03 23:56:18 +1100

    create Page and Link class files

---
+++ site_generator_scripts/Link.py
@@ -0,0 +1,6 @@
+class Link:
+    def __init__(self, title, link, description, tags):
+        self.title = title
+        self.link = link
+        self.description = description
+        self.tags = tags
---
+++ site_generator_scripts/Page.py
@@ -0,0 +1,6 @@
+class Page:
+    def __init__(self, category, title, description, content = None):
+        self.category = category
+        self.title = title
+        self.description = description
+        self.content = content
